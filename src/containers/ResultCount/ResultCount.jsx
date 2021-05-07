2a1d9b8cdc922f4bfd58e2a08e9851cc3756e0a0
Aliaksandr Patapeika <aliaksandr_patapeika@epam.com>
2021-05-07 13:43:09 +0300

    Task11: Add reselect

--- src/containers/ResultCount/ResultCount.jsx
+++ src/containers/ResultCount/ResultCount.jsx
@@ -2,14 +2,20 @@ import './ResultCount.less';
 
 import React from 'react';
 import {useSelector} from 'react-redux';
+import {createSelector} from 'reselect';
 
+const totalAmountSelector = (totalAmount) => totalAmount;
+const getTotalAmount = createSelector(
+  totalAmountSelector,
+  (totalAmount) => totalAmount
+);
 const ResultCount = () => {
   const totalAmount = useSelector((state) => state.movie.totalAmount);
 
   return (
     <div className="resultCountContainer">
       <span className="resultCount">
-        {totalAmount || 0}
+        {getTotalAmount(totalAmount) || 0}
         {' '}
       </span>
       <span>movies found</span>
